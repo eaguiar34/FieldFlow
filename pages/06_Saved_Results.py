e7df8fa9b52300cd6f5346c64247a0055b90cc83
eaguiar34 <emilianoaguiar34@gmail.com>
2026-03-01 16:11:54 -0800

    Remove page icon from Saved Results configuration

--- pages/06_Saved_Results.py
+++ pages/06_Saved_Results.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import fieldflow_core as core
 
-st.set_page_config(page_title="Saved Results", page_icon="\U0001f5c2", layout="wide")
+st.set_page_config(page_title="Saved Results", page_icon=None, layout="wide")
 core.render_sidebar("Saved Results")
 core.saved_results_page()
